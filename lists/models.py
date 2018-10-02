9af4f8bc0cac884e7987886b1a3ca814b7dec8ae
GiJDog <gi_joey7@hotmail.com>
2018-10-02 18:33:44 -0500

    Model for list Items and associated migration

--- lists/models.py
+++ lists/models.py
@@ -1,5 +1,5 @@
 from django.db import models
 
 class Item(models.Model):
-    text = models.TextField()
+    text = models.TextField(default='')
 # Create your models here test- resume at "A New Field Means a New Migration".
